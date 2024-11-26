28cd8f2e0e9e5ced457372775dcd57381b7bc6f5
Lee Ha Eun <233haeun@gmail.com>
2024-11-26 13:49:38 +0900

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: haelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 15:42:15 by haelee            #+#    #+#             */
-/*   Updated: 2024/11/20 18:18:51 by haelee           ###   ########.fr       */
+/*   Updated: 2024/11/26 13:44:27 by haelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,19 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	j;
+	size_t	str_len;
 	char	*sub;
 
-	i = 0;
-	j = 0;
-	if (s == NULL)
+	if (!s)
 		return (NULL);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len - start;
 	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (sub == NULL)
+	if (!sub)
 		return (NULL);
-	while (s[i] != '\0')
-	{
-		if (i >= start && j < len)
-		{
-			sub[j] = s[i];
-			j++;
-		}
-		i++;
-		sub[j] = '\0';
-	}
+	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
